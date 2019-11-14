7c54c3b807ba123493203d1d16df17038a803c10
Shantanu Raj <s@sraj.me>
2019-11-14 16:14:51 +0530

    feat(core): Init barebones XCode project

---
+++ Podcst/Podcst/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Podcst
+//
+//  Created by Shantanu Raj on 14/11/19.
+//  Copyright © 2019 Shantanu Raj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
